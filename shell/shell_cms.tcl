e41ee1136bfd31b2fd08944e610f4d8712fa3f3e
Joan Teruel (jteruel) <joan.teruel@bsc.es>
2024-07-05 16:32:38 +0200

    Solved TCL bugs

--- shell/shell_cms.tcl
+++ shell/shell_cms.tcl
@@ -17,53 +17,53 @@
 # Description: CMS implementation precondition: HBM
 
 putwarnings "CMS enabled, needed HBM memory"
-putmeeps "Creating CMS..."
-#Create the CMS, no properties needed
-create_bd_cell -type ip -vlnv xilinx.com:ip:cms_subsystem:4.0 cms_subsystem_0
 
-set PortList [lappend PortList $g_cms_file]
+#Create the CMS, no properties needed
+create_bd_cell -type ip -vlnv xilinx.com:ip:cms_subsystem:4.0 cms_subsystem
 
 save_bd_design
 
 #Connect temp sensors data 1 and 2
-connect_bd_net [get_bd_pins hbm_0/DRAM_0_STAT_TEMP] [get_bd_pins cms_subsystem_0/hbm_temp_1] 
-connect_bd_net [get_bd_pins hbm_0/DRAM_1_STAT_TEMP] [get_bd_pins cms_subsystem_0/hbm_temp_2]
+connect_bd_net [get_bd_pins hbm_0/DRAM_0_STAT_TEMP] [get_bd_pins cms_subsystem/hbm_temp_1] 
+connect_bd_net [get_bd_pins hbm_0/DRAM_1_STAT_TEMP] [get_bd_pins cms_subsystem/hbm_temp_2]
 
 #Connect ored cattrip
-connect_bd_net [get_bd_pins hbm_cattrip_or/Res] [get_bd_pins cms_subsystem_0/interrupt_hbm_cattrip]
+connect_bd_net [get_bd_pins hbm_cattrip_or/Res] [get_bd_pins cms_subsystem/interrupt_hbm_cattrip]
 
 #We need a 50Mhz clock for the aclk_ctrl, chipset_clk is connected to clock wizard out 1 which creates a 50Mhz 
-connect_bd_net [get_bd_pins cms_subsystem_0/aclk_ctrl] [get_bd_ports chipset_clk]
+connect_bd_net [get_bd_pins cms_subsystem/aclk_ctrl] [get_bd_ports chipset_clk]
 
-#We need a aresetn signal
-connect_bd_net [get_bd_pins cms_subsystem_0/aresetn_ctrl] [get_bd_pins rst_ea_CLK0/peripheral_aresetn]
+#We need a aresetn signal which will be connected to a Processor System reset
+#Setup the cms_reset
+create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 cms_reset
+connect_bd_net [get_bd_pins cms_reset/ext_reset_in] [get_bd_ports pcie_perstn]
+connect_bd_net [get_bd_pins cms_reset/slowest_sync_clk] [get_bd_pins clk_wiz_1/clk_out1]
+connect_bd_net [get_bd_pins cms_subsystem/aresetn_ctrl] [get_bd_pins cms_reset/peripheral_aresetn]
 
 #Create GPIO port (width of 4)
-create_bd_port -dir I -from 3 -to 0 -type intr satellite_gpio
+make_bd_pins_external  [get_bd_pins cms_subsystem/satellite_gpio]
+set_property name satellite_gpio [get_bd_ports satellite_gpio_0]
 
-#Create an input GPIO port for the SC
+#Make external UART connection
+make_bd_intf_pins_external [get_bd_intf_pins cms_subsystem/satellite_uart]
+
+#Connect with AXI pcie lite interconnect
+#First modify interconnect to make space to fit another master
 set_property -dict [list \
- CONFIG.SENSITIVITY [get_property CONFIG.SENSITIVITY [get_bd_pins cms_subsystem_0/satellite_gpio]] \
- CONFIG.PortWidth [get_property CONFIG.PortWidth [get_bd_pins cms_subsystem_0/satellite_gpio]] \
-] [get_bd_ports satellite_gpio]
+  CONFIG.NUM_MI {2} \
+] [get_bd_cells axi_xbar_pcie_lite]
+set_property CONFIG.pf0_bar2_size_qdma {512} [get_bd_cells qdma_0]
 
-connect_bd_net [get_bd_pins /cms_subsystem_0/satellite_gpio] [get_bd_ports satellite_gpio]
+#Connect M01 aclk and areset signals
+connect_bd_net [get_bd_pins axi_xbar_pcie_lite/M01_ACLK] [get_bd_pins clk_wiz_1/clk_out1]
+connect_bd_net [get_bd_pins axi_xbar_pcie_lite/M01_ARESETN] [get_bd_pins cms_reset/interconnect_aresetn]
 
-#Create an output UART TXD port
-create_bd_port -dir O satellite_uart_txd
-connect_bd_net [get_bd_pins /cms_subsystem_0/satellite_uart_txd] [get_bd_ports satellite_uart_txd]
+#Connect M01 to s_axi_ctrl from CMS
+connect_bd_intf_net -boundary_type upper [get_bd_intf_pins axi_xbar_pcie_lite/M01_AXI] [get_bd_intf_pins cms_subsystem/s_axi_ctrl]
 
-#Create an input UART RXD port
-create_bd_port -dir I satellite_uart_rxd
-connect_bd_net [get_bd_pins /cms_subsystem_0/satellite_uart_rxd] [get_bd_ports satellite_uart_rxd]
+#We'll leave the 'interrupt_host' signal unconnected
 
-delete_bd_objs [get_bd_intf_nets axi_xbar_pcie_M00_AXI] [get_bd_cells SHELL_ROM]
-connect_bd_intf_net -boundary_type upper [get_bd_intf_pins axi_xbar_pcie/M00_AXI] [get_bd_intf_pins cms_subsystem_0/s_axi_ctrl]
-disconnect_bd_net /proc_sys_rst_pcie_peripheral_aresetn [get_bd_pins axi_xbar_pcie/M00_ARESETN]
-disconnect_bd_net /qdma_0_axi_aclk [get_bd_pins axi_xbar_pcie/M00_ACLK]
-connect_bd_net [get_bd_pins axi_xbar_pcie/M00_ACLK] [get_bd_pins clk_wiz_1/clk_out1]
-connect_bd_net [get_bd_pins axi_xbar_pcie/M00_ARESETN] [get_bd_pins rst_ea_CLK0/peripheral_aresetn]
-
-#What do we do with 'interrupt_host'?
+#At BAR2 we can find at [0x0,0x4000) INFO ROM and at [0x4_0000, 0x8_0000) the CMS reserved space
+assign_bd_address -offset 0x40000 -target_address_space /qdma_0/M_AXI_LITE [get_bd_addr_segs cms_subsystem/s_axi_ctrl/Mem] -force
 
 putmeeps "CMS created"
